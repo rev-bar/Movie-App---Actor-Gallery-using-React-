1741606e46d37a4390d9cdf5929d41264c25ebcc
rev bar <revital.jer@gmail.com>
2020-12-29 18:31:30 +0200

    Rendering search component layout

--- src/pages/ActorPage.js
+++ src/pages/ActorPage.js
@@ -1,4 +1,4 @@
-import { Container, Row } from 'react-bootstrap';
+import { Container, Form, Row } from 'react-bootstrap';
 import ActorCard from '../components/ActorCard/ActorCard';
 import './ActorPage.css';
 
@@ -6,9 +6,27 @@ import './ActorPage.css';
 function ActorsPage(){
 
 return (
-<Container className= "p-actors ">
+<Container className= "p-actors">
+<Form.Group >
+    <Form.Control as="textarea" className= "p-actors-search" placeholder= "Search" />
+  </Form.Group>
+
     <Row className= "flex">
         <ActorCard
+        fname ="Gal" 
+        lname = "Gadot"
+        imageUrl =   "https://m.media-amazon.com/images/M/MV5BMjUzZTJmZDItODRjYS00ZGRhLTg2NWQtOGE0YjJhNWVlMjNjXkEyXkFqcGdeQXVyMTg4NDI0NDM@._V1_UY317_CR51,0,214,317_AL_.jpg"
+        imdbUrl= "https://www.imdb.com/name/nm2933757/"
+        age = "40"   
+        />
+                <ActorCard
+        fname ="Gal" 
+        lname = "Gadot"
+        imageUrl =   "https://m.media-amazon.com/images/M/MV5BMjUzZTJmZDItODRjYS00ZGRhLTg2NWQtOGE0YjJhNWVlMjNjXkEyXkFqcGdeQXVyMTg4NDI0NDM@._V1_UY317_CR51,0,214,317_AL_.jpg"
+        imdbUrl= "https://www.imdb.com/name/nm2933757/"
+        age = "40"   
+        />
+                <ActorCard
         fname ="Gal" 
         lname = "Gadot"
         imageUrl =   "https://m.media-amazon.com/images/M/MV5BMjUzZTJmZDItODRjYS00ZGRhLTg2NWQtOGE0YjJhNWVlMjNjXkEyXkFqcGdeQXVyMTg4NDI0NDM@._V1_UY317_CR51,0,214,317_AL_.jpg"

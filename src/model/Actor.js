c70d386ab2f7873ac58a45db0cbe6008f320901d
rev bar <revital.jer@gmail.com>
2020-12-29 10:12:18 +0200

    Empty Actors Page component + Actor class ver 1 (no age calc)

---
+++ src/model/Actor.js
@@ -0,0 +1,12 @@
+class Actor {
+    constructor(fname,lname,imageUrl,imdbUrl,age) {
+        this.fname = fname;
+        this.lname = lname;    
+        this.imageUrl = imageUrl;
+        this.imdbUrl = imdbUrl;
+        this.age = age;
+        }
+  
+}
+
+export default Actor;
---
+++ src/pages/ActorPage.js
@@ -0,0 +1,16 @@
+import './ActorPage.css';
+
+
+function ActorsPage(){
+
+return (
+
+<div>
+    actor page test
+</div>
+
+)
+
+}
+
+export default ActorsPage;
